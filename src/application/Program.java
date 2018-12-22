23171b10e6969265a7fd7db3f29435f8e9acc566
sergioru <sergio_ru@hotmail.com>
2018-12-22 11:21:07 -0200

    Projeto Criado

---
+++ src/application/Program.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package application;
+import java.util.Scanner;
+import java.util.Locale;
+
+/**
+ *
+ * @author sergi
+ */
+public class Program {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(new Locale("en", "US"));
+
+
+
+
+
+
+        sc.close();
+    }
+
+}
